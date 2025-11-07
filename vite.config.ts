acc3c41fec5d77765256cb4ce09c3b41d0d87bcb
Oliver Mak <oliver.mak@naxos.com>
2025-11-07 15:21:27 +0800

    Fix error

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,12 @@
 import { fileURLToPath, URL } from 'node:url'
 
-import vue from '@vitejs/plugin-vue'
+import { PrimeVueResolver } from '@primevue/auto-import-resolver'
+import { VueUseComponentsResolver, VueUseDirectiveResolver } from 'unplugin-vue-components/resolvers'
 
 import AutoImport from 'unplugin-auto-import/vite'
-import { VueUseComponentsResolver, VueUseDirectiveResolver } from 'unplugin-vue-components/resolvers'
 import Components from 'unplugin-vue-components/vite'
 
+import vue from '@vitejs/plugin-vue'
 import { defineConfig } from 'vite'
 import vueDevTools from 'vite-plugin-vue-devtools'
 
